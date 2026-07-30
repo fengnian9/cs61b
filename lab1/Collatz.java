04c4890ab5d040f96b2604b788af88e3f1babe49
fengnian9 <wal132444565@gmail.com>
2026-07-30 22:49:06 +0800

    fixed lab1

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -3,15 +3,14 @@
  */
 public class Collatz {
 
-    /** Buggy implementation of nextNumber! */
+    /**
+     * Buggy implementation of nextNumber!
+     */
     public static int nextNumber(int n) {
-        if (n  == 128) {
-            return 1;
-        } else if (n == 5) {
-            return 3 * n + 1;
-        } else {
-            return n * 2;
+        if (n % 2 == 0) {
+            return n / 2;
         }
+        return (3 * n + 1);
     }
 
     public static void main(String[] args) {
@@ -24,4 +23,5 @@ public class Collatz {
         System.out.println();
     }
 }
+
 

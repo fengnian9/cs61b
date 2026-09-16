b7f97eab112c7220ec07ad233dbfe261bf5f48ae
fengnian9 <wal132444565@gmail.com>
2026-09-16 18:14:55 +0800

    feat: first draft of BSTMap

---
+++ lab7/bstmap/BSTMap.java
@@ -0,0 +1,112 @@
+package bstmap;
+
+import java.util.Iterator;
+import java.util.Set;
+
+public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
+
+    private Node rootNode;
+    private int size;
+
+    private class Node {
+        K key;
+        V value;
+        Node leftChild;
+        Node rightChild;
+
+        Node(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+
+    BSTMap(K key, V value) {
+        this.rootNode = null;
+        this.size = 0;
+    }
+
+    @Override
+    public void clear() {
+        rootNode = null;
+        size = 0;
+    }
+
+    @Override
+    public boolean containsKey(K key) {
+        Node node = search(rootNode, key);
+        return node != null;
+    }
+
+    private Node search(Node node, K key) {
+        if (node == null) {
+            return null;
+        }
+
+        int cmp = key.compareTo(node.key);
+        if (cmp < 0) {
+            return search(node.leftChild, key);
+        } else if (cmp > 0) {
+            return search(node.rightChild, key);
+        } else {
+            return node;
+        }
+
+    }
+
+    @Override
+    public V get(K key) {
+        Node node = search(rootNode,key);
+        if (node.value == null) {
+            return null;
+        }
+        return node.value;
+    }
+
+    @Override
+    public int size() {
+        return this.size;
+    }
+
+    private Node put(Node node, K key, V value) {
+        if (node == null) {
+            size += 1;
+            return new Node(key, value);
+        }
+
+        int cmp = key.compareTo(node.key);
+        if (cmp < 0) {
+            node.leftChild = put(node.leftChild, key, value);
+        } else if (cmp > 0) {
+            node.rightChild = put(node.rightChild, key, value);
+        } else {
+            node.value = value;
+        }
+        return node;
+    }
+
+    @Override
+    public void put(K key, V value) {
+        Node root = put(rootNode, key, value);
+    }
+
+    @Override
+    public Set<K> keySet() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public V remove(K key) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public V remove(K key, V value) {
+        throw new UnsupportedOperationException();
+
+    }
+
+    @Override
+    public Iterator<K> iterator() {
+        throw new UnsupportedOperationException();
+    }
+}
